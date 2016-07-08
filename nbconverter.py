753f2c2e778a08ac2abc0e13d0f6f946edccc545
Ryan May <rmay31@gmail.com>
2016-07-08 17:21:02 -0600

    Fix some paths in conversion.

--- nbconverter.py
+++ nbconverter.py
@@ -52,6 +52,8 @@ if __name__ == '__main__':
             img_file = write_nb(notebook_source_dir, *nb_to_markdown(fname))
             filename = os.path.split(fname)[1]
             if not img_file:
-                img_file = 'website/images/placeholder.png'
-            test.write('<a href="notebooks/' + filename + '"><img alt="' + filename.replace('_', ' ').replace('.ipynb', '') +
-                       '"src="' + img_file + '" height="300" width="375"></a>\n')
+                img_file = 'images/placeholder.png'
+            else:
+                img_file = os.path.relpath(img_file, 'website')
+            test.write('<a href="' + filename.replace('ipynb', 'html') + '"><img alt="' + filename.replace('_', ' ').replace('.ipynb', '') +
+                       '" src="' + img_file + '" height="300" width="375"></a>\n')
