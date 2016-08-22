847e862959f705f757820f030d66b5ede8e359e4
Ryan May <rmay31@gmail.com>
2016-08-22 15:46:26 -0600

    MNT: Fix markdown index.
    GitHub changed markedown renderers, which slightly broke our index file.

--- nbconverter.py
+++ nbconverter.py
@@ -26,7 +26,7 @@ def make_thumbnail(nb_path):
 
 if __name__ == '__main__':
     with open(os.path.join(SITE_DIR, 'index.md'), 'w') as index:
-        index.write('---\ntitle: Unidata\'s Notebook Gallery\n--- \n#Notebook Gallery\n')
+        index.write('---\ntitle: Unidata\'s Notebook Gallery\n--- \n# Notebook Gallery\n')
         base_url = 'http://nbviewer.jupyter.org/github/unidata/notebook-gallery/blob/master/'
         for fname in glob.glob(os.path.join('notebooks', '*.ipynb')):
             print('Converting %{0} -> '.format(fname))
