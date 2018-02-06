8582b1528f15f85b9814b39a9bfed2b27574bc08
J.R. Leeman <kd5wxb@gmail.com>
2018-02-06 09:10:24 -0700

    Add build step to Travis for running on THREDDS-test.

--- thredds_test_converter.py
+++ thredds_test_converter.py
@@ -3,14 +3,14 @@
 import fileinput
 from pathlib import Path
 
-
+print('Converting examples to use thredds-test.ucar.edu')
 # Recursively grab examples
 scripts = Path('examples').rglob('*.py')
 
 # Go through each script, looking for http://thredds.ucar.edu and replacing
 # it with http://thredds-test.unidata.ucar.edu to test against
 search_text = 'http://thredds.ucar.edu'
-replace_text = 'http://thredds-test.unidata.ucar.edu'
+replace_text = 'http://threddds-test.unidata.ucar.edu'
 
 for script in scripts:
     print('Processing {}'.format(str(script)))
