32ffc25e57149348b10d886f2421d0e3d597bed9
Aodhan Sweeney <ajs15e@my.fsu.edu>
2019-07-29 09:05:18 -0600

    Adding spc Tracker and the hurricane tracker with small updates.

--- examples/HurricaneTracker.py
+++ examples/HurricaneTracker.py
@@ -5,16 +5,17 @@ By: Aodhan Sweeney
 
 This GUI takes data pulled from the National
 Hurricane Center and plots specific storms and
-their tracks. The GUI runs using ipwidgets, and
-is intended to be used as an ipython notebook.
+their tracks. The GUI runs using ipywidgets, and
+is intended to be used as an Jupyter notebook.
 """
 
 import cartopy.crs as ccrs
+from IPython.display import display
 import ipywidgets as widgets
 import matplotlib.pyplot as plt
 import numpy as np
-from pandas import DataFrame
 from siphon.simplewebservice import nhc
+
 
 class NHC_GUI:
     """
@@ -44,7 +45,6 @@ class NHC_GUI:
                                                  disabled=False, button_style='info',
                                                  tooltip='Description')
         widgets.interact(self.get_track, track_button=self.track_button)
-
 
     def get_storms_slider(self, year_slider):
         """
@@ -77,7 +77,7 @@ class NHC_GUI:
         if self.filename.empty is False:
             self.filename = file_name[0]
         elif self.filename.empty is True:
-            raise Exception('ValueError: No file name data for this year.')
+            raise ValueError('No data for specific file name of the chosen.')
 
     def get_track(self, track_button):
         """
@@ -109,10 +109,10 @@ class NHC_GUI:
         self.NHCD.model_selection_latlon(models)
         self.date_times = self.NHCD.date_times.tolist()
         self.plot_slider = widgets.IntSlider(min=0, max=(len(self.date_times)-1),
-                                                 value=0, description='Tracks Time',
-                                                 disabled=False)
+                                             value=0, description='Tracks Time',
+                                             disabled=False)
         self.play = widgets.Play(interval=800, min=0, max=(len(self.date_times)-1),
-                                                 value=0, description='Tracks Time')
+                                 value=0, description='Tracks Time')
         widgets.jslink((self.plot_slider, 'value'), (self.play, 'value'))
         self.box = widgets.HBox([self.plot_slider, self.play])
         display(self.plot_slider)
@@ -144,8 +144,7 @@ class NHC_GUI:
             min_best_lon = min(self.best_lons)
             max_best_lon = max(self.best_lons)
 
-
-            #Plotting the tracks on top of a cartopy stock image projection
+            # Plotting the tracks on top of a cartopy stock image projection
             self.fig = plt.figure(figsize=(14, 11))
             self.ax = self.fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
             self.ax.stock_img()
@@ -156,8 +155,6 @@ class NHC_GUI:
             self.ax.set_extent([(min_best_lon - 40), (max_best_lon + 40),
                                 (min_best_lat - 40), (max_best_lat + 40)])
 
-
-
             jet = plt.get_cmap('jet')
             colors = iter(jet(np.linspace(0.2, 1, (len(self.model_select.value)+1))))
             left = .1
@@ -166,16 +163,17 @@ class NHC_GUI:
                          fontsize=14, color='black')
 
             for model_type in self.NHCD.model_table:
-                one_model_time = model_type[model_type['WarnDT'] == self.date_times[plot_slider]]
-                lats = one_model_time['Lat'].tolist()
-                lons = one_model_time['Lon'].tolist()
+                one_time = model_type[model_type['WarnDT'] == self.date_times[plot_slider]]
+                lats = one_time['Lat'].tolist()
+                lons = one_time['Lon'].tolist()
                 if len(lats) != 0:
                     model_list = model_type['Model'].tolist()
                     self.ax.plot(lons, lats, marker='o', color=next(colors),
                                  label=model_list[0])
             plt.title('Storm Name: {0} Year: {1}'.format(self.storm_names.value,
-                                                        self.year))
+                                                         self.year))
             plt.legend()
+
 
 ######################################################################
 NHC_GUI()
